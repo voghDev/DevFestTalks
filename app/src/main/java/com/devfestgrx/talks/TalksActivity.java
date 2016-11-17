1b08b5aebac1aa33f418d9eff8956b6c81d4b1f2
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-11-17 17:50:37 +0100

    Initial commit

---
+++ app/src/main/java/com/devfestgrx/talks/TalksActivity.java
@@ -0,0 +1,13 @@
+package com.devfestgrx.talks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TalksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talks);
+    }
+}
