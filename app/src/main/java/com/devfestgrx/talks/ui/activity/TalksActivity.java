91072d7e349dc48d9843e43ef22050bb835098b4
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-11-19 12:37:35 +0100

    Step 1 completed. Empty GetTalks. Next: DI

--- app/src/main/java/com/devfestgrx/talks/ui/activity/TalksActivity.java
+++ app/src/main/java/com/devfestgrx/talks/ui/activity/TalksActivity.java
@@ -2,9 +2,17 @@ package com.devfestgrx.talks.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.devfestgrx.talks.R;
+import com.devfestgrx.talks.global.model.Talk;
 import com.devfestgrx.talks.ui.presenter.TalksPresenter;
+import com.devfestgrx.talks.usecase.GetTalks;
+
+import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
 
@@ -12,13 +20,19 @@ public class TalksActivity extends BaseActivity implements TalksPresenter.MVPVie
     @Bind(R.id.talks_recyclerView)
     RecyclerView recyclerView;
 
+    @Bind(R.id.talks_pbr_loading)
+    ProgressBar pbrLoading;
+
     TalksPresenter presenter;
+
+    //@Inject
+    GetTalks getTalks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        presenter = new TalksPresenter(this);
+        presenter = new TalksPresenter(this, getTalks);
         presenter.setView(this);
         presenter.setNavigator(this);
 
@@ -28,5 +42,27 @@ public class TalksActivity extends BaseActivity implements TalksPresenter.MVPVie
     @Override
     protected int getLayoutId() {
         return R.layout.activity_talks;
+    }
+
+    @Override
+    public void showLoading() {
+        pbrLoading.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void hideLoading() {
+        pbrLoading.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void showError(String message) {
+        // TODO implement error handling
+    }
+
+    @Override
+    public void showTalks(List<Talk> list) {
+        // TODO implement
     }
 }
--- app/src/main/java/com/devfestgrx/talks/ui/presenter/TalksPresenter.java
+++ app/src/main/java/com/devfestgrx/talks/ui/presenter/TalksPresenter.java
@@ -2,19 +2,46 @@ package com.devfestgrx.talks.ui.presenter;
 
 import android.content.Context;
 
+import com.devfestgrx.talks.R;
+import com.devfestgrx.talks.global.model.Talk;
+import com.devfestgrx.talks.usecase.GetTalks;
+
+import java.util.List;
+
 /**
  * Created by olmo on 17/11/16.
  */
 public class TalksPresenter extends Presenter<TalksPresenter.MVPView, TalksPresenter.Navigator> {
     Context context;
+    GetTalks getTalks;
 
-    public TalksPresenter(Context context) {
+    public TalksPresenter(Context context, GetTalks getTalks) {
         this.context = context;
+        this.getTalks = getTalks;
     }
 
     @Override
     public void initialize() {
+        view.showLoading();
+        getTalks.getTalks(new GetTalks.Listener() {
+            @Override
+            public void onTalksCompleted(List<Talk> list) {
+                view.showTalks(list);
+                view.hideLoading();
+            }
 
+            @Override
+            public void onError(Exception e) {
+                view.showError(e.getMessage());
+                view.hideLoading();
+            }
+
+            @Override
+            public void onNoInternetAvailable() {
+                view.showError(context.getString(R.string.no_internet));
+                view.hideLoading();
+            }
+        });
     }
 
     @Override
@@ -34,6 +61,13 @@ public class TalksPresenter extends Presenter<TalksPresenter.MVPView, TalksPrese
 
     public interface MVPView extends AbsMVPView {
 
+        void showLoading();
+
+        void hideLoading();
+
+        void showError(String message);
+
+        void showTalks(List<Talk> list);
     }
 
     public interface Navigator extends AbsNavigator {
