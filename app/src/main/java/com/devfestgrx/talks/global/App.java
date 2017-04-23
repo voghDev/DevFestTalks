e97bd196320ffed62aadd56c8bfd2ad79310b892
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-04-23 18:09:33 +0200

    Fix dagger and wrap_content

--- app/src/main/java/com/devfestgrx/talks/global/App.java
+++ app/src/main/java/com/devfestgrx/talks/global/App.java
@@ -2,6 +2,7 @@ package com.devfestgrx.talks.global;
 
 import android.app.Application;
 
+import com.devfestgrx.talks.global.di.DaggerRootComponent;
 import com.devfestgrx.talks.global.di.MainModule;
 import com.devfestgrx.talks.global.di.RootComponent;
 
@@ -22,9 +23,9 @@ public class App extends Application {
 
     private void initializeDependencyInjection() {
         mainModule = new MainModule(this);
-//        component = DaggerRootComponent.builder()
-//                .mainModule(mainModule)
-//                .build();
+        component = DaggerRootComponent.builder()
+                .mainModule(mainModule)
+                .build();
         component.inject(this);
     }
 
